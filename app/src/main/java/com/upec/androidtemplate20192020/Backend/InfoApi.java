f8fc86030263cdf2d72b0d915ae8257eb5880189
Lissossi Cédric <cedric.lissossi@gmail.com>
2020-07-08 16:11:23 +0200

    Update InfoApi.java

--- app/src/main/java/com/upec/androidtemplate20192020/Backend/InfoApi.java
+++ app/src/main/java/com/upec/androidtemplate20192020/Backend/InfoApi.java
@@ -1,10 +1,9 @@
 package com.upec.androidtemplate20192020.Backend;
 
 public interface InfoApi {
-    //String SNCF_API_KEY="306acd22-47a7-4bc7-8f97-a940d1e3b4c0";
-    String SNCF_API_KEY="3bb79fa0-9932-4eaf-89ff-6a3c6aa12710";
+    String SNCF_API_KEY="PUT_YOUR_SNCF_APIKEY";
     String SNCF_API_URL="https://api.sncf.com/v1/";
-    String SNCF_API_KEY2="2af0c57f-7db5-47bb-ba7a-a726db38b335";
+    String SNCF_API_KEY2="PUT_YOUR_SNCF_APIKEY";
 
 
 
